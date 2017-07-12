8f66bdefe8a1a47856e40d8edf5e4f683e898088
Trevor <tgahl7@gmail.com>
2017-07-12 14:27:35 -0600

    Commented

--- pyMaestro.py
+++ pyMaestro.py
@@ -2,11 +2,8 @@ import serial
 import serial.tools.list_ports
 import time
 
-running = True
 
 class Maestro:
-
-    #s = serial.Serial('COM14', 9600, timeout=0.5)
 
     def __init__(self):
         comPort = self.searchComPorts()
@@ -16,6 +13,9 @@ class Maestro:
     def close(self):
         self.usb.close()
 
+    ##########################################
+    ## Autodetect COM Port for Mini-Maestro ##
+    ##########################################
     def searchComPorts(self):
         ports = list(serial.tools.list_ports.comports())
         for each in ports:
@@ -33,11 +33,17 @@ class Maestro:
                     print servoCOM
                     return servoCOM
 
+    ################################################
+    ## Move to target position (8-bit resolution) ##
+    ################################################
     def setTargetMiniSSC(self, channel, value):
         command = [0xFF, channel, value]
         print(command)
         self.usb.write(command)
 
+    #################################################
+    ## Move to target position (14-bit resolution) ##
+    #################################################
     def setTargetCompact(self, channel, value):
         msb = (value >> 7) & 0x7F
         lsb = value & 0x7F
@@ -45,6 +51,9 @@ class Maestro:
         print(command)
         self.usb.write(command)
 
+    #########################################
+    ## Reads in last sent positional value ##
+    #########################################
     def getPosition(self, channel):
         command = [0x90, channel]
         self.usb.write(command)
@@ -54,18 +63,27 @@ class Maestro:
         value = (msb << 8) | lsb
         print value
 
+    ############################
+    ## Sets servo speed value ##
+    ############################
     def setSpeed(self, channel, value):
         msb = (value >> 7) & 0x7F
         lsb = value & 0x7F
         command = [0x87, channel, lsb, msb]
         self.usb.write(command)
 
+    ###################################
+    ## Sets servo acceleration value ##
+    ###################################
     def setAcceleration(self, channel, value):
         msb = (value >> 7) & 0x7F
         lsb = value & 0x7F
         command = [0x89, channel, lsb, msb]
         self.usb.write(command)
 
+    ###########################################
+    ## Reads in any error codes from Maestro ##
+    ###########################################
     def getErrors(self):
         command = [0xA1]
         self.usb.write(command)
@@ -89,23 +107,32 @@ class Maestro:
         else:
             print "No errors"
 
+    ####################################
+    ## Method to test library methods ##
+    ####################################
     def initTest(self):
         self.setSpeed(1, 1)
         self.setAcceleration(1, 1)
-        self.setTargetCompact(1,6000)
+        self.setTargetCompact(1, 6000)
         self.getPosition(1)
         self.getErrors()
-        self.setTargetCompact(1,5000)
-        self.setTargetCompact(1,6000)
+        self.setTargetCompact(1, 5000)
+        self.setTargetCompact(1, 6000)
 
+
+###################################################################
+## Example usage of library with a simple command line interface ##
+###################################################################
+running = True
 maestro = Maestro()
 maestro.initTest()
+
 while running:
     print "\nEnter desired PWM value or 0 to exit"
     inValue = int(raw_input())
     if inValue == 0:
         running = False
     else:
-        maestro.setTargetCompact(1,inValue)
+        maestro.setTargetCompact(1, inValue)
         print (inValue / 4)
         maestro.getPosition(1)
